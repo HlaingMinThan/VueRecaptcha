2ab6884a3cd47c9a4fb8ba3952936c2cb50982cf
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-05-18 10:31:24 +0700

    recaptcha simple work

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form action="" @submit.prevent="submit">
+      <input type="text" placeholder="email" v-model="email" />
+      <input type="text" placeholder="password" v-model="password" />
+      <div
+        id="g-recaptcha"
+        data-size="invisible"
+        :data-sitekey="sitekey"
+      ></div>
+
+      <button type="submit">submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      email: "",
+      password: "",
+      sitekey: "6LfY2-YfAAAAAMohJHhdVnaE02hV0GnOd-QHGNVg",
+    };
+  },
+  methods: {
+    getToken(token) {
+      this.reset();
+      console.log(token);
+    },
+    reset(){
+      window.grecaptcha.reset();
+    },
+    submit() {
+      window.grecaptcha.execute();
+    },
+    render() {
+      setTimeout(() => {
+        if (
+          typeof window.grecaptcha === "undefined" ||
+          typeof window.grecaptcha.render === "undefined"
+        ) {
+          this.render();
+        } else {
+          window.grecaptcha.render("g-recaptcha", {
+            sitekey: this.sitekey,
+            callback: (token) => {
+              this.getToken(token);
+            },
+            // "expired-callback": this.expired,
+          });
+        }
+      }, 100);
+    },
+  },
+  mounted() {
+    this.render();
+  },
+};
 </script>
 
 <style>
