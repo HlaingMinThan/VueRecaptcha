0d96d9a5eab25d65ac2b2b0ebc13b77f7382edb7
HlaingMinThan <hlaingminthan@92@gmail.com>
2022-05-20 14:25:02 +0700

    update prop

--- vue-simple-recaptcha/dev/serve.vue
+++ vue-simple-recaptcha/dev/serve.vue
@@ -12,7 +12,7 @@ export default Vue.extend({
 
 <template>
   <div id="app">
-    <vue-simple-recaptcha :sitekey="sitekey"/>
+    <vue-simple-recaptcha :sitekey="sitekey" :invisible="invisible"/>
   </div>
 </template>
 <script>
@@ -21,6 +21,10 @@ export default {
     sitekey: {
       type: String,
       required: true,
+    },
+    invisible: {
+      type: Boolean,
+      default:false
     },
   },
 }
